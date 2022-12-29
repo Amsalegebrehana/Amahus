7711b53a7fe3cb06c62dc338a2f5f785ce62a078
AmanuelD02 <amanueldebebe1000@gmail.com>
2022-12-29 22:38:00 +0300

    feat: assign and demote representative CRUD

--- api/class/controller.js
+++ api/class/controller.js
@@ -1,11 +1,12 @@
 
 // class dal
 const Class = require("./dal");
+// user dal
+const User = require("../user/dal");
 // AppError
 const AppError = require("../../utils/appError");
 // pagination
 const pagination = require("../../utils/pagination");
-const User = require("../user/model");
 
 
 exports.fetchClasses = async (req, res, next) => {
@@ -93,9 +94,128 @@ exports.updateClassById = async (req, res, next) => {
 
 
 exports.assignRepresentative = async (req, res, next) => {
+    try{
+        const {classId, userId} = req.params;
+        const kifel = await Class.fetchClassById(classId);
+
+        // check if id is correct
+        if(!kifel){
+            return next(
+                new AppError("There is no Class with the specified id", 400)
+              );
+        }
+
+        // check if class have rep already assigned
+        if (kifel.representativeId != null){
+            return next(
+                new AppError("Representative is already assigned", 400)
+            );
+        }
+        
+
+        const user = await User.getUserById(userId);
+        // check if user is not null
+        if(!user){
+            return next(
+                new AppError("There is no user with the specified id", 400)
+              );
+        }
+        
+
+        // Check if user is already rep
+        if (user.role == "Representative") {
+            return next(
+                new AppError("User already representative", 400)
+              );
+        }
+
+        // check if user is in another class
+        if (user.classId != kifel.id) {
+            return next(
+                new AppError("User is in another class", 400)
+              );
+        }
+
+        // update user
+        const updatedUser = await User.updateUser(userId, {role: "Representative" });
+
+        // update class
+        const updatedClass = await Class.updateClassById(classId, {representativeId: userId})
+
+
+        
+        res.status(201).json({
+            status:"Success",
+            data:updatedClass,
+         });
+        
+
+    } catch(error) {
+        next(error);
+    }
 
 }
 
 exports.demoteRepresentative  = async (req, res, next) => {
-    
+    try {  
+        
+        const classId = req.params.classId;
+        const kifel = await Class.fetchClassById(classId);
+
+        // check if id is correct
+        if(!kifel){
+            return next(
+                new AppError("There is no Class with the specified id", 400)
+              );
+        }
+
+        // check if class have no rep already assigned
+        if (kifel.representativeId == null){
+            return next(
+                new AppError("Class does not have a representative", 400)
+            );
+        }
+        
+        const userId = kifel.representativeId;
+
+        const user = await User.getUserById(userId);
+
+        // check if user is not null
+        if(!user){
+            return next(
+                new AppError("There is no user with the specified id", 400)
+              );
+        }
+        
+
+        // Check if user is not already rep
+        if (user.role != "Representative") {
+            return next(
+                new AppError("User is not representative", 400)
+              );
+        }
+        
+        // check if user is in another class
+        if (user.classId != kifel.id) {
+            return next(
+                new AppError("User is in another class", 400)
+              );
+        }
+
+        // update user
+        const updatedUser = await User.updateUser(userId, {role: "Member" });
+
+        // update class
+        const updatedClass = await Class.updateClassById(classId, {representativeId: null})
+
+
+        
+        res.status(201).json({
+            status:"Success",
+            data:updatedClass,
+         });
+
+    } catch(error){
+        next(error);
+    }
 }
--- api/class/router.js
+++ api/class/router.js
@@ -14,5 +14,7 @@ router.route("/").get(classController.fetchClasses).post(classController.createC
 
 router.route("/:id").get(classController.fetchClassById).put(classController.updateClassById);
 
+router.route("/:classId/assign/:userId").post(classController.assignRepresentative);
+router.route("/:classId/demote").put(classController.demoteRepresentative);
 
 module.exports = router;
