698524819a35d7280f92d280fd33059f8fd0b14f
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2023-03-06 22:43:58 +0300

    Update router.js

--- api/user/router.js
+++ api/user/router.js
@@ -1,20 +1,6 @@
 
 // endpoints
-/**
- * 
- * getAll users
- * CRUD   UserById
- * get Users By Class
- * filter users by name, batch, department, class
- *
- * assign yekiflat teteri by user id
- * create, get all kiflteteri
- * get all kiflat
- * demote by user id -- from kifl teteri to member
- * demote all reps
- * 
- * 
- */
+
 
 // Express
 const express = require("express");
